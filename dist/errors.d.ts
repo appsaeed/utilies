786add8e8bfd17e56a8ac2c5cd5c267c7daaed25
Saeed <78552486+appsaeed@users.noreply.github.com>
2024-04-22 18:35:30 +0600

    Delete dist directory

--- dist/errors.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function errorToString(error: any): string;
-export declare function errorsToString(...errors: any[]): string;
-export declare const extractError: typeof errorToString;
-export declare const extractErrors: typeof errorsToString;
--- dist/proto.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function haskey<O, K extends keyof O>(this: O, key: K): boolean;
-export declare function in_array(search: string[] | string, arr: []): boolean;
