888e864bfbf7e0850ed8ac9b54acfdc19abf1d65
Saeed Hossen <appsaeed7@gmail.com>
2024-05-22 07:06:52 +0600

    update yml

--- main.js
+++
@@ -1,9 +0,0 @@
-import { decryptSync, encryptSync } from './dist/encryption.js';
-
-//encrypted text using secret key
-const encrypted = encryptSync('Hello world!', 'my_secret')
-console.log('Encrypted: ', encrypted)//Encrypted:  JRwzHwpDBQoGAR1+
-
-//decrypted text using secret key and encrypted
-const decrypted = decryptSync('JRwzHwpDBQoGAR1+', 'my_secret')
-console.log('Decrypted: ', decrypted) //Decrypted:  Hello world!
